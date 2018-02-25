3e5d33b8f38c5616502dbee125b9a4a7052d2d07
Cathy <cathy.chang.ct@gmail.com>
2018-02-24 18:22:49 -0800

    Primer Done
    Primer class is done!

--- Athenahacks/app/src/main/java/com/example/cathychang/athenahacks/Primer.java
+++ Athenahacks/app/src/main/java/com/example/cathychang/athenahacks/Primer.java
@@ -1,13 +1,54 @@
 package com.example.cathychang.athenahacks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
-public class Primer extends AppCompatActivity {
+
+public class Primer extends Ultimate {
+
+    public Primer()
+    {
+        super() ;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primer);
+        Button primer = (Button)findViewById(R.id.nextPrimer);
+
+        primer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent= new Intent(Primer.this, Contour.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onClick(View view){
+        boolean checked =((CheckBox)view).isChecked();
+        switch (view.getId()){
+            case R.id.face:
+                if (checked)
+                    primerFace=true;
+                break;
+            case R.id.eyeshadow:
+                if (checked)
+                    primerEyes=true;
+                break;
+            case R.id.none:
+                if (checked){
+                    primerFace=false;
+                    primerEyes=false;
+                    primerNone=true;
+                }
+
+        };
     }
 }
