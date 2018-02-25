1164c52c7c8c12b44a73c4c366e018827dedfae2
Rakia Segev <rakiasegev@github.com>
2018-02-24 22:11:20 -0800

    eyeshadow

--- Athenahacks/app/src/main/java/com/example/cathychang/athenahacks/MainActivity.java
+++ Athenahacks/app/src/main/java/com/example/cathychang/athenahacks/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
 
     }
 
