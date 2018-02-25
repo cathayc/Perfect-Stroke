197b9b598975ea0b9145954bfdd566ed45a78a39
snadgauda <33081418+snadgauda@users.noreply.github.com>
2018-02-25 11:52:48 -0800

    Revert "update video ultimate"
    This reverts commit 5d24a3c9efaf5dc000e2f932f1ea78acc8346496.

--- Athenahacks/app/src/main/java/com/example/cathychang/athenahacks/Ultimate.java
+++ Athenahacks/app/src/main/java/com/example/cathychang/athenahacks/Ultimate.java
@@ -1,13 +1,12 @@
 package com.example.cathychang.athenahacks;
 
-import java.util.ArrayList;
+import android.support.v7.app.AppCompatActivity;
 
-//package com.example.cathychang.athenahacks;
 /**
  * Created by shriyanadgauda on 2/24/18.
  */
 
-public class Ultimate {
+public class Ultimate extends AppCompatActivity {
 
     boolean foundationLiquid;
     boolean foundationPowder;
@@ -44,157 +43,129 @@ public class Ultimate {
     boolean shadowYellow;
     boolean shadowWhite;
 
-    ArrayList<Video> videos;
+    Video [] videos;
 
     public Ultimate()
     {
-        this.foundationLiquid = false;
-        this.foundationPowder = false;
-        this.foundationNone = false;
+        foundationLiquid = false;
+        foundationPowder = false;
+        foundationNone = false;
 
-        this.primerFace = false;
-        this.primerEyes = false;
-        this.primerNone = false;
+        primerFace = false;
+        primerEyes = false;
+        primerNone = false;
 
-        this.contourYes = false;
-        this.contourNo = false;
+        contourYes = false;
+        contourNo = false;
 
-        this.blushYes = false;
-        this.blushNo = false;
+        blushYes = false;
+        blushNo = false;
 
-        this.eyelinerDaily = false;
-        this.eyelinerCat = false;
-        this.eyelinerNightOut = false;
-        this.eyelinerNone = false;
+        eyelinerDaily = false;
+        eyelinerCat = false;
+        eyelinerNightOut = false;
+        eyelinerNone = false;
 
-        this.mascaraYes = false;
-        this.mascaraNo = false;
+        mascaraYes = false;
+        mascaraNo = false;
 
-        this.eyebrowsArc = false;
-        this.eyebrowsGel = false;
-        this.eyebrowsPencil = false;
-        this.eyebrowsNone = false;
+        eyebrowsArc = false;
+        eyebrowsGel = false;
+        eyebrowsPencil = false;
+        eyebrowsNone = false;
 
-        this.shadowBlack = false;
-        this.shadowPurple = false;
-        this.shadowBlue = false;
-        this.shadowGold = false;
-        this.shadowRed = false;
-        this.shadowYellow = false;
-        this.shadowWhite = false;
-
-        makeVideos();
-
+        shadowBlack = false;
+        shadowPurple = false;
+        shadowBlue = false;
+        shadowGold = false;
+        shadowRed = false;
+        shadowYellow = false;
+        shadowWhite = false;
     }
 
-
     public void makeVideos() {
-
-        videos = new ArrayList<Video> ();
-
+        videos = new Video[6];
         boolean[] temp0 = {true, false, false, true, false,
                 false, false, false, true, false, false, false,
                 false, false, true, false, false, false,
                 false, false, false, false, false, false, false, false, false} ;
-        Video vid = new Video( temp0, "https://www.youtube.com/watch?v=-WlqBjKC0C8");
-        videos.add(vid)  ;
+        videos[0] = new Video( temp0, "https://www.youtube.com/watch?v=-WlqBjKC0C8");
 
         boolean[] temp1 = {true, false, false, true, false,
                 false, true, false, true, false, true, false,
                 false, false, true, false, true, true,
                 true, true, false, false, false, false, false, false, false};
 
-        Video vid1 = new Video( temp1, "https://www.youtube.com/watch?v=-WlqBjKC0C8");
-        videos.add( vid1)  ;
+        videos[1] = new Video( temp1, "https://www.youtube.com/watch?v=-WlqBjKC0C8");
 
         boolean[] temp2 = {true, false, false, true, false,
                 false, true, false, true, false, true, false,
                 false, false, true, false, false, false,
                 true, false, true, true, false, true, false, false, false};
-
-        Video vid2 = new Video(temp2, "https://www.youtube.com/watch?v=I_h_ssTeLPw");
-        videos.add(vid2)  ;
+        videos[2] = new Video(temp2, "https://www.youtube.com/watch?v=I_h_ssTeLPw");
 
         boolean[] temp3 = {false, false, false, false, false,
                 false, true, false, true, false, false, true,
                 false, false, true, false, false, false,
                 false, false, false, true, false, true, false, false, false };
 
-        Video vid3 = new Video( temp3, "https://www.youtube.com/watch?v=2dcbxPN_OH0");
-        videos.add( vid3)  ;
+        videos[3] = new Video( temp3, "https://www.youtube.com/watch?v=2dcbxPN_OH0");
 
         boolean[] temp4 = {false, true, false, true, false,
                 false, true, false, false, false, false, false,
                 true, false, true, false, false, true,
                 true, false, false, false, true, true, false, false, false};
 
-        Video vid4 = new Video(temp4, "https://www.youtube.com/watch?v=4xt-zjxVGNc");
-        videos.add(vid4)  ;
+            videos[4] = new Video(temp4, "https://www.youtube.com/watch?v=4xt-zjxVGNc");
 
         boolean[] temp5 = {true, false, false, true, false,
                 false, true, false, true, false, false, false,
                 false, false, false, false, false, false,
                 true, false, false, false, false, true, false, false, false};
 
-        Video vid5 = new Video(temp4, "https://www.youtube.com/watch?v=uKR1_Npz87Q");
-        videos.add( vid5)  ;
+        videos[5] = new Video(temp5, "https://www.youtube.com/watch?v=uKR1_Npz87Q");
 
 
-        boolean[] temp6 = {false, false, false, false, false,
-                false, false, false, false, false, false, false,
-                false, false, false, false, false, false,
-                false, false, false, false, false, false, false, false, false};
-
-        Video vid6 = new Video(temp6, "https://www.youtube.com/watch?v=ZoGe4m_W9_Y");
-        videos.add( vid6)  ;
-
-
-    }
+        }
     /*Finds the video in videos that matches with the current unltimate the most and then returns
         the url for that video*/
     public String bestVideo()
     {
-        boolean[] ultimateStats = new boolean [27];
-        ultimateStats[0] = this.foundationPowder;
-        ultimateStats[1] = this.foundationLiquid;
-        ultimateStats[2] = this.foundationNone;
-        ultimateStats[3] = this.primerFace;
-        ultimateStats[4] = this.primerEyes;
-        ultimateStats[5] = this.primerNone;
-        ultimateStats[6] = this.contourYes;
-        ultimateStats[7] = this.contourNo;
-        ultimateStats[8] = this.blushYes;
-        ultimateStats[9] = this.blushNo;
-        ultimateStats[10] = this.eyelinerDaily;
-        ultimateStats[11] = this.eyelinerCat;
-        ultimateStats[12] = this.eyelinerNightOut;
-        ultimateStats[13] = this.eyelinerNone;
-        ultimateStats[14] = this.mascaraYes;
-        ultimateStats[15] = this.mascaraNo;
-        ultimateStats[16] = this.eyebrowsArc;
-        ultimateStats[17] = this.eyebrowsGel;
-        ultimateStats[18] = this.eyebrowsPencil;
-        ultimateStats[19] = this.eyebrowsNone;
-        ultimateStats[20] = this.shadowBlack;
-        ultimateStats[21] = this.shadowPurple;
-        ultimateStats[22] = this.shadowBlue;
-        ultimateStats[23] = this.shadowGold;
-        ultimateStats[24] = this.shadowRed;
-        ultimateStats[25] = this.shadowYellow;
-        ultimateStats[26] = this.shadowWhite;
+        boolean[] ultimateStats = new boolean [21];
+        ultimateStats[0] = foundationPowder;
+        ultimateStats[1] = foundationLiquid;
+        ultimateStats[2] = foundationNone;
+        ultimateStats[3] = primerFace;
+        ultimateStats[4] = primerEyes;
+        ultimateStats[5] = primerNone;
+        ultimateStats[6] = contourYes;
+        ultimateStats[7] = contourNo;
+        ultimateStats[8] = blushYes;
+        ultimateStats[9] = blushNo;
+        ultimateStats[10] = eyelinerDaily;
+        ultimateStats[11] = eyelinerCat;
+        ultimateStats[12] = eyelinerNightOut;
+        ultimateStats[13] = eyelinerNone;
+        ultimateStats[14] = mascaraYes;
+        ultimateStats[15] = mascaraNo;
+        ultimateStats[16] = eyebrowsArc;
+        ultimateStats[17] = eyebrowsGel;
+        ultimateStats[18] = eyebrowsPencil;
+        ultimateStats[19] = eyebrowsNone;
+        ultimateStats[20] = shadowBlack;
 
         int maxCompare = 0;
         int index = 0;
-        for(int i = 0; i < videos.size(); i++){
-            int temp=  compare(videos.get(i).getVideo(), ultimateStats) ;
+        for(int i = 0; i < videos.length; i++){
+            int temp=  compare(videos[i].getVideo(), ultimateStats) ;
             if(temp > maxCompare)
             {
                 maxCompare = temp;
                 index = i;
             }
-        }
+         }
 
-        return videos.get(index).getUrl()  ;
+        return videos[index].getUrl()  ;
     }
 
     /*
@@ -211,16 +182,6 @@ public class Ultimate {
             }
         }
         return compared;
-
-    }
-
-    public static void main(String[] args)
-    {
-        Ultimate test = new Ultimate ();
-        String vid = test.bestVideo();
-        System.out.println(vid);
-
-
     }
 
 }
